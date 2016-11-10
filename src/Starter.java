1f54a66258bc85df023b8caac2cbae9e4a550e99
Estudiante <Estudiante@FI454402-20.unal.loc>
2016-11-10 16:19:48 -0500

    Shuffle

---
+++ src/Starter.java
@@ -0,0 +1,42 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Starter {
+
+    
+    public static void menu (){
+        
+        System.out.println("1.Remover una mascota");
+        System.out.println("2.Mascotas con dueño identificado");
+        System.out.println("3. Lista de mascotas");
+                System.out.println("Gatos");
+                System.out.println("Perros");
+                System.out.println("Hamsters");
+        System.out.println("3. Lista de mascotas");
+        System.out.println("4.Mascotas registradas en veterinaria");
+        System.out.println("Exit");
+       
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        
+     
+        
+        Scanner imput = new Scanner(new File("pets.txt"));
+        
+        public void readPets (Vet v);
+        public void manageVet (Vet v, String action);
+    }
+    
+}
